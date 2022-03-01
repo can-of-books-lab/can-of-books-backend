9eb8c459ac20c818b5e3e903639b6edba94a9274
Andres Mills Gallego <geneseewoodworking@gmail.com>
2022-03-01 15:57:05 -0800

    finished the post request on the server

--- Models/Book.js
+++ Models/Book.js
@@ -5,10 +5,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const bookSchema = new Schema ({
-  title: {type: String, required: true},
-  description: {type: String, required: true},
+  title: {type: String, required: false},
+  description: {type: String, required: false},
   status: {type: Boolean, required: false},
-  email: {type: String, required: true}
+  email: {type: String, required: false}
 });
 
 const BookModel = mongoose.model('Book', bookSchema);
